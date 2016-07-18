bcf938a795387fb19aaa3002b04e9d86919e1e10
pselle <pamela.selle@gmail.com>
2016-07-18 15:09:44 -0400

    remove map and js from files

--- django-rest-angular2-example/angular2/app/app.component.ts
+++ django-rest-angular2-example/angular2/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { DinosaurComponent } from './components/dinosaur/dinosaur'
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: '<h1>Demo Angular App!</h1><dinosaur></dinosaur>'
 })
 export class AppComponent { }
