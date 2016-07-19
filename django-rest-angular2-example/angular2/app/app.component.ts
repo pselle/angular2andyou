ad73e17a2ecbd98ebfabbcffc10f973efab043cc
pselle <pamela.selle@gmail.com>
2016-07-18 21:07:03 -0400

    Move sample to its own repo

--- django-rest-angular2-example/angular2/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import { DinosaurComponent } from './components/dinosaur/dinosaur'
-
-@Component({
-  selector: 'my-app',
-  template: '<h1>Demo Angular App!</h1><dinosaur></dinosaur>'
-})
-export class AppComponent { }
